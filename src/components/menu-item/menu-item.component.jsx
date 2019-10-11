2d682b2bc250ee43877feb5e562d7a98ec478ca8
Martins Benoit <benoit.martins@outlook.com>
2019-10-11 12:33:48 +0200

    Improve menu-item styling : - Add zoom effect on the background image - Add google font - Change text capitalize from state to css style

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -9,6 +9,12 @@ const MenuItem = ({ title, imageUrl, size }) => (
     }}
     className={`${size} menu-item`}
   >
+    <div
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+      className='background-image'
+    ></div>
     <div className='content'>
       <h1 className='title'>{title}</h1>
       <span className='subtitle'>BUY ME</span>
