2f16ed2c49ce37b2dc3d63be55ab080eed6569df
Martins Benoit <benoit.martins@outlook.com>
2019-10-11 11:00:57 +0200

    Add item and style to home page :
    - Update class name to match the styles file
    - Import styles file from udemy course
    - Add new items to the skeleton

--- src/components/homepage.component.jsx
+++ src/components/homepage.component.jsx
@@ -1,12 +1,38 @@
 import React from "react";
 
+import './homepage.styles.scss'
+
 const HomePage = () => (
   <div className='homepage'>
-    <div className='showcase-menu'>
+    <div className='directory-menu'>
       <div className='menu-item'>
-        <div className='item-content'>
-          <h1 className='title'>Stuff</h1>
-          <span className='subtitle'>A kind of stuff</span>
+        <div className='content'>
+          <h1 className='title'>Stuff 1</h1>
+          <span className='subtitle'>BUY ME</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Stuff 2</h1>
+          <span className='subtitle'>BUY ME</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>Stuff 3</h1>
+          <span className='subtitle'>BUY ME</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>The Stuff</h1>
+          <span className='subtitle'>BUY NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>The Thing</h1>
+          <span className='subtitle'>BUY NOW</span>
         </div>
       </div>
     </div>
