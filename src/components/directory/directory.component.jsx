f9d19a5e84a8e898a9c5cbcd711dcde9126bcabc
Martins Benoit <benoit.martins@outlook.com>
2019-10-12 12:28:38 +0200

    Add routing mechanics to the app :
    - Add router to the root coponent
    - Add url link to section data
    - Create small page to test routing
    - Nest menu-item in router higher comp to add router props

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,24 +11,28 @@ class Directory extends React.Component {
         {
           id: 1,
           title: 'cards games',
+          linkUrl: 'cardsgames',
           imageUrl:
             'https://www.forbiddenpower.com/wp-content/uploads/2016/08/Mtg-logo-700x560.png'
         },
         {
           id: 2,
-          title: 'war games',
+          title: 'wars games',
+          linkUrl: 'warsgames',
           imageUrl:
             'https://images.unsplash.com/photo-1529981188441-8a2e6fe30103?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
         },
         {
           id: 3,
           title: 'role playing games',
+          linkUrl: 'rpgs',
           imageUrl:
             'https://images.unsplash.com/photo-1546070609-877c13504085?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1348&q=80'
         },
         {
           id: 4,
-          title: 'boardgames',
+          title: 'board games',
+          linkUrl: 'boardgames',
           imageUrl:
             'https://images.unsplash.com/photo-1563941402830-3bae42b67b38?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
           size: 'large'
@@ -36,6 +40,7 @@ class Directory extends React.Component {
         {
           id: 5,
           title: 'video games',
+          linkUrl: 'videogames',
           imageUrl:
             'https://images.unsplash.com/photo-1539716947714-3295e1074d33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
           size: 'large'
@@ -47,15 +52,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ id, title, imageUrl, size }) => {
-          return (
-            <MenuItem
-              id={id}
-              title={title}
-              imageUrl={imageUrl}
-              size={size}
-            ></MenuItem>
-          );
+        {this.state.sections.map(({ id, ...otherSectionPros }) => {
+          return <MenuItem key={id} {...otherSectionPros}></MenuItem>;
         })}
       </div>
     );
