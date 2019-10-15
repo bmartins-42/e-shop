0b3fd2a326d7dd11d0a9e0a8613ba651c7beae27
Martins Benoit <benoit.martins@outlook.com>
2019-10-15 23:42:25 +0200

    Create collection preview :
    - Create function component to wrap category shop item display
    - Add css given by udemy courses
    - Add collections to shop page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 
 import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component.jsx";
 
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
 
-    this.shopData = {
+    this.state = {
       collections: SHOP_DATA
     };
   }
 
   render() {
-    return (<div>
-      <h1>SHOP PAGE</h1>
-    </div>)
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview
+            key={id}
+            {...otherCollectionProps}
+          ></CollectionPreview>
+        ))}
+      </div>
+    );
   }
 }
 
