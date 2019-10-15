acfdfdfa5c82e92b8ef8a428f915b915577a6150
Martins Benoit <benoit.martins@outlook.com>
2019-10-15 23:19:20 +0200

    Create Shop Page :
    - Remove quick routing test with boardgame page
    - Create new shop page component
    - Add ShopData from Udemy course

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import SHOP_DATA from "./shop.data";
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.shopData = {
+      collections: SHOP_DATA
+    };
+  }
+
+  render() {
+    return (<div>
+      <h1>SHOP PAGE</h1>
+    </div>)
+  }
+}
+
+export default ShopPage;
