b1c64214bf9b72fd329bf06da4361a10fd97e35d
Martins Benoit <benoit.martins@outlook.com>
2019-10-18 00:06:11 +0200

    Fix issue with wrong div name

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 
 import './header.styles.scss';
 import { Link } from 'react-router-dom';
-import {ReactComponent as Logo } from '../../assets/boardgame.icon.svg';
+import { ReactComponent as Logo } from '../../assets/boardgame.icon.svg';
 
 const Header = () => (
   <div className='header'>
-    <Link to='' className='logo-container'>
-      <Logo className='logo'/>
+    <Link to='/' className='logo-container'>
+      <Logo className='logo' />
     </Link>
-    <Link className='options'>
+    <div className='options'>
       <Link className='option' to='/shop'>
         shop
       </Link>
-      <Link className='option' to='/contact'>
-        contact
+      <Link className='option' to='/login'>
+        login
       </Link>
-    </Link>
+    </div>
   </div>
 );
 
